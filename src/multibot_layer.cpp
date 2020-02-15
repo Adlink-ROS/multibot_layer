7853b1ea1e5c762f3db56f449107e15a7c0822bd
airuchen <B10307115@gapps.ntust.edu.tw>
2020-02-15 14:22:16 +0800

    Clean code

--- src/multibot_layer.cpp
+++ src/multibot_layer.cpp
@@ -1,94 +1,84 @@
 #include<multibot_layers/multibot_layer.h>
 #include <pluginlib/class_list_macros.h>
- 
+
 PLUGINLIB_EXPORT_CLASS(multibot_layer_namespace::MultibotLayer, costmap_2d::Layer)
- 
-using costmap_2d::LETHAL_OBSTACLE;
- 
-namespace multibot_layer_namespace
+
+	using costmap_2d::LETHAL_OBSTACLE;
+
+	namespace multibot_layer_namespace
 {
+	MultibotLayer::MultibotLayer() {}
+
+	void MultibotLayer::onInitialize()
+	{
+		ros::NodeHandle nh("~/" + name_);
+		current_ = true;
+
+		dsrv_ = new dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>(nh);
+		dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>::CallbackType cb = boost::bind(
+				&MultibotLayer::reconfigureCB, this, _1, _2);
+		dsrv_->setCallback(cb);
+	}
 
 
-MultibotLayer::MultibotLayer() {}
- 
-void MultibotLayer::onInitialize()
-{
-  ros::NodeHandle nh("~/" + name_);
-  current_ = true;
+	void MultibotLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level)
+	{
+		enabled_ = config.enabled;
+	}
 
-  dsrv_ = new dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>(nh);
-  dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>::CallbackType cb = boost::bind(
-      &MultibotLayer::reconfigureCB, this, _1, _2);
-  dsrv_->setCallback(cb);
-}
- 
- 
-void MultibotLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level)
-{
-  enabled_ = config.enabled;
-}
- 
-void MultibotLayer::updateBounds(double origin_x, double origin_y, double origin_yaw, double* min_x,
-                                           double* min_y, double* max_x, double* max_y)
-{
-  if (!enabled_)
-    return;
+	void MultibotLayer::updateBounds(double origin_x, double origin_y, double origin_yaw, double* min_x,
+			double* min_y, double* max_x, double* max_y)
+	{
+		if (!enabled_)
+			return;
 
-  
-  nh.setParam("pose_x", origin_x);
-  nh.setParam("pose_y", origin_y);
 
-  // dummy search for five robot. TODO: set dynamic robot array
-  for (int i = 0; i < 5; i++) {
-    std::string robot_pose = "/tb3_";
-    std::string robot_x;
-    std::string robot_y;
-    std::string this_bot;
-    // search the full parameter name of pose of the current robot. Eg, this_bot = "tb3_<current_robot_number>/pose_x".
-    nh.searchParam("pose_x", this_bot);  
+		nh.setParam("pose_x", origin_x);
+		nh.setParam("pose_y", origin_y);
 
-  
-    robot_pose.append(std::to_string(i));  // set robot_pose to "/tb3_<i=1~5>"
-    robot_x = robot_y = robot_pose;  // give robot_x and robot_y the same prefix
-    robot_x.append("/pose_x");  // set robot_x to "/tb3_<i>/pose_x"
-    robot_y.append("/pose_y");  // set robot_y to "/tb3_<i>/pose_y"
-    
-    // Save poses of every robot, except itself, in mark[][] array
-    if (nh.searchParam(robot_x, robot_x) && robot_x != this_bot) {
-      nh.getParam(robot_x, mark[i][0]);
-      nh.getParam(robot_y, mark[i][1]);
-    } else {
-      mark[i][0] = 100;  // Set an unaffected pose for unused robots
-      mark[i][1] = 100;  // Set an unaffected pose for unused robots
-    } 
+		// dummy search for five robots. TODO: set dynamic robot array
+		for (int i = 0; i < 5; i++) {
+			std::string robot_pose = "/tb3_";
+			std::string robot_x;
+			std::string robot_y;
+			std::string this_bot;
+			// search for the full posename of the current robot. Eg, this_bot = "tb3_<current_robot_number>/pose_x".
+			nh.searchParam("pose_x", this_bot);  
 
-    *min_x = std::min(*min_x, mark[i][0]);
-    *min_y = std::min(*min_y, mark[i][1]);
-    *max_x = std::max(*max_x, mark[i][0]);
-    *max_y = std::max(*max_y, mark[i][1]);
-  }
-}
- 
-void MultibotLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i,
-                                          int max_j)
-{
-  if (!enabled_)
-    return;
-  unsigned int mx;
-  unsigned int my;
-  for (int i = 0; i < 5; i++) {
-    if(master_grid.worldToMap(mark[i][0], mark[i][1], mx, my)){
-    
-      if (mx-3 < 0) mx = 3;
-      if (my-3 < 0) my = 3;
-      //   for (int x = 0; x < 6; x++) {
-      //     for (int y = 0; y < 6; y++) {
-      //      master_grid.setCost(mx-3+x, my-3+y, LETHAL_OBSTACLE);
-      master_grid.setCost(mx, my, LETHAL_OBSTACLE);
-      //     }
-      //   } 
-    }
-  }
-} 
+
+			robot_pose.append(std::to_string(i));  // set robot_pose to "/tb3_<i=1~5>"
+			robot_x = robot_y = robot_pose;  // give robot_x and robot_y the same prefix
+			robot_x.append("/pose_x");  // set robot_x to "/tb3_<i>/pose_x"
+			robot_y.append("/pose_y");  // set robot_y to "/tb3_<i>/pose_y"
+
+			// Save poses of every robot, except itself, in mark[][] array
+			if (nh.searchParam(robot_x, robot_x) && robot_x != this_bot) {
+				nh.getParam(robot_x, mark[i][0]);
+				nh.getParam(robot_y, mark[i][1]);
+			} else {
+				mark[i][0] = 100;  // Set an unaffected pose for unused robots
+				mark[i][1] = 100;  // Set an unaffected pose for unused robots
+			} 
+
+			*min_x = std::min(*min_x, mark[i][0]);
+			*min_y = std::min(*min_y, mark[i][1]);
+			*max_x = std::max(*max_x, mark[i][0]);
+			*max_y = std::max(*max_y, mark[i][1]);
+		}
+	}
+
+	void MultibotLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i,
+			int max_j)
+	{
+		if (!enabled_)
+			return;
+		unsigned int mx;
+		unsigned int my;
+		for (int i = 0; i < 5; i++) {
+			if(master_grid.worldToMap(mark[i][0], mark[i][1], mx, my)){
+				master_grid.setCost(mx, my, LETHAL_OBSTACLE);
+			}
+		}
+	} 
 
 } // end namespace
