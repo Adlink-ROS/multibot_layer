259f024983a249a058ef4e3f95da8d7639ed302b
airuchen <B10307115@gapps.ntust.edu.tw>
2020-02-15 14:15:00 +0800

    Rename the whole pkg

--- include/multibot_layers/multibot_layer.h
+++ include/multibot_layers/multibot_layer.h
@@ -1,32 +1,30 @@
-#ifndef GRID_LAYER_H_
-#define GRID_LAYER_H_
+#ifndef MULTIBOT_LAYER_H_
+#define MULTIBOT_LAYER_H_
 #include <ros/ros.h>
 #include <costmap_2d/layer.h>
 #include <costmap_2d/layered_costmap.h>
 #include <costmap_2d/GenericPluginConfig.h>
 #include <dynamic_reconfigure/server.h>
-
-namespace simple_layer_namespace
+ 
+namespace multibot_layer_namespace
 {
 
-class GridLayer : public costmap_2d::Layer, public costmap_2d::Costmap2D
+class MultibotLayer : public costmap_2d::Layer
 {
 public:
-  GridLayer();
-
+  MultibotLayer();
+ 
   virtual void onInitialize();
-  virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y, double* max_x,
+  virtual void updateBounds(double origin_x, double origin_y, double origin_yaw, double* min_x, double* min_y, double* max_x,
                              double* max_y);
   virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
-  bool isDiscretized()
-  {
-    return true;
-  }
-
-  virtual void matchSize();
-
+ 
 private:
   void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
+
+  
+  double mark[10][2];
+  ros::NodeHandle nh;
   dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv_;
 };
 }
--- src/multibot_layer.cpp
+++ src/multibot_layer.cpp
@@ -1,34 +1,34 @@
-#include<simple_layers/simple_layer.h>
+#include<multibot_layers/multibot_layer.h>
 #include <pluginlib/class_list_macros.h>
  
-PLUGINLIB_EXPORT_CLASS(simple_layer_namespace::SimpleLayer, costmap_2d::Layer)
+PLUGINLIB_EXPORT_CLASS(multibot_layer_namespace::MultibotLayer, costmap_2d::Layer)
  
 using costmap_2d::LETHAL_OBSTACLE;
  
-namespace simple_layer_namespace
+namespace multibot_layer_namespace
 {
 
 
-SimpleLayer::SimpleLayer() {}
+MultibotLayer::MultibotLayer() {}
  
-void SimpleLayer::onInitialize()
+void MultibotLayer::onInitialize()
 {
   ros::NodeHandle nh("~/" + name_);
   current_ = true;
 
   dsrv_ = new dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>(nh);
   dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>::CallbackType cb = boost::bind(
-      &SimpleLayer::reconfigureCB, this, _1, _2);
+      &MultibotLayer::reconfigureCB, this, _1, _2);
   dsrv_->setCallback(cb);
 }
  
  
-void SimpleLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level)
+void MultibotLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level)
 {
   enabled_ = config.enabled;
 }
  
-void SimpleLayer::updateBounds(double origin_x, double origin_y, double origin_yaw, double* min_x,
+void MultibotLayer::updateBounds(double origin_x, double origin_y, double origin_yaw, double* min_x,
                                            double* min_y, double* max_x, double* max_y)
 {
   if (!enabled_)
@@ -69,7 +69,7 @@ void SimpleLayer::updateBounds(double origin_x, double origin_y, double origin_y
   }
 }
  
-void SimpleLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i,
+void MultibotLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i,
                                           int max_j)
 {
   if (!enabled_)
